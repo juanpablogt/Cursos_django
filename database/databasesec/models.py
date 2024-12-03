b64e8dc853c152c972a8a0a97f42ee1a7d33dfbd
Jgonzalez <juapabgonzalez@utp.edu.co>
2024-12-02 22:22:31 -0500

    Base de datos

--- database/databasesec/models.py
+++ database/databasesec/models.py
@@ -4,3 +4,7 @@ from django.db import models
 
 class Post(models.Model):
     text = models.TextField()
+
+#permite que se muestre el texto en el admin
+    def __str__(self):
+        return self.text[:50]
