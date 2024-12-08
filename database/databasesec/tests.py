d1e43d85c36e4751150db9dc2bc1a86cadf12603
Jgonzalez <juapabgonzalez@utp.edu.co>
2024-12-07 19:19:30 -0500

    Testing

--- database/databasesec/tests.py
+++ database/databasesec/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .models import Post
+
+class PostTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.post = Post.objects.create(text='just a test')
+
+    def test_model_content(self):
+        self.assertEqual(self.post.text, 'just a test')
