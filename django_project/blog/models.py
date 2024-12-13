93fcdc3d9cc38312b72f2e3d3a8abd1f812e5770
Jgonzalez <juapabgonzalez@utp.edu.co>
2024-12-12 20:06:08 -0500

    enlazando bd con templete, mostrar contenido de bd en un template.

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -11,6 +11,7 @@ class Post(models.Model):
         on_delete=models.CASCADE,
     )
     body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
