272bfca69117329d8045025171c436cd51ca2727
juanpablogt <juapabgonzalez@utp.edu.co>
2024-12-15 20:32:00 -0500

    editando los post

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from .models import Post
 
 class BlogListView(ListView):
@@ -18,3 +18,8 @@ class BlogCreateView(CreateView):
     model = Post
     template_name = 'post_new.html'
     fields = ['title', 'author', 'body']
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
