4dae7cbf6976bd3e1b50a6a0390105df0b41de81
juanpablogt <juapabgonzalez@utp.edu.co>
2024-12-20 01:20:23 -0500

    Inicio de sesion y crear usuario

---
+++ django_project/accounts/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+
+class SignUpView(CreateView):
+    template_name = 'registration/signup.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+
+
